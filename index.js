7caf23fd187f16ab46fddfb4812856d4fa2386d6
EErickson <avilasonson@gmail.com>
2023-04-11 08:37:15 +0800

    fix

---
+++ index.js
@@ -0,0 +1,10 @@
+var firstValue = 100;
+    var formula = '10 + firstValue ';
+    //while(true){}
+    document.addEventListener('DOMContentLoaded', function () {
+        const input = document.getElementById('inputT');
+        const result = document.getElementById('result');
+
+        input.innerText = formula;
+        result.innerText = eval(formula)
+});
