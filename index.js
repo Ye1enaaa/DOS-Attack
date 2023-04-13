ca9beafa835d920165bbcfeb62d19ec4329d817c
EErickson <avilasonson@gmail.com>
2023-04-13 08:38:20 +0800

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var firstValue = 100;
     var formula = '10 + firstValue; while(true){} ';
-    //while(true){}
+    //while(true){}//
     document.addEventListener('DOMContentLoaded', function () {
         const input = document.getElementById('inputT');
         const result = document.getElementById('result');
